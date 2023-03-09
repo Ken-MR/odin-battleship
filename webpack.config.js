4ab1be03d66cbaaf82e001b3b200e530e81b8f65
KenMR <kmr5591@gmail.com>
2023-03-08 23:10:48 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/page-control.js',
+  devtool: 'inline-source-map',
+  output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist/'),
+      clean: true,
+	},
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      ],
+  },
+};
