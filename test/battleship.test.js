130cd1c71f875ef3539d914da74dc13a9ec916ff
KenMR <kmr5591@gmail.com>
2023-03-09 22:13:43 -0500

    Create destroyer object and check it sinks sooner than a battleship

--- test/battleship.test.js
+++ test/battleship.test.js
@@ -4,7 +4,7 @@ const battleship = new gameLogic.ShipCreator('battleship');
 
 test('Create a battleship object', () => {
   expect(new gameLogic.ShipCreator('battleship')).toBeDefined()
-})
+});
 
 test('Check shipType parameter for battleship', () => {
   expect(battleship.shipType).toEqual('battleship');
@@ -29,4 +29,20 @@ test('Check if the ship is sunk after multiple hits', () => {
   battleship.hit();
   battleship.hit();
   expect(battleship.sunk).toBeTruthy();
+});
+
+const destroyer = new gameLogic.ShipCreator('destroyer');
+
+test('Create a destroyer object', () => {
+  expect(new gameLogic.ShipCreator('destroyer')).toBeDefined()
+});
+
+test('Check shipType parameter for battleship', () => {
+  expect(destroyer.shipType).toEqual('destroyer');
+});
+
+test('Check if the ship is sunk after multiple hits', () => {
+  destroyer.hit();
+  destroyer.hit();
+  expect(destroyer.sunk).toBeTruthy();
 });
