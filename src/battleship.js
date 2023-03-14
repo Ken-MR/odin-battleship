56124e4d505a6fb3572aa1ce11228d00cd566b01
KenMR <kmr5591@gmail.com>
2023-03-14 19:37:42 -0400

    Create ship placement and creation function for the gameboard and verify it works with appropriate tests

--- src/battleship.js
+++ src/battleship.js
@@ -30,3 +30,68 @@ class ShipCreator {
   };
 }
 module.exports.ShipCreator = ShipCreator;
+
+
+
+class Space {
+  // create a new space and parameters to track whether it was hit, its coordinates,
+  // whether a ship is located there, etc
+  constructor(coordinates) {
+    this.struck = null;
+    this.occupied = null;
+    this.space = coordinates;
+  }
+}
+
+class GameBoard {
+  constructor(name) {
+    this.board = this.generateBoard();
+    this.lost = false;
+    this.name = name;
+    this.fleet = [];
+  }
+
+  generateBoard() {
+    // 2d array of board spaces
+    let board = [[],[],[],[],[],[],[],[]];
+    for (let i = 0; i < 7; i++) {
+      for (let j = 0; j < 7; j++) {
+        board[i].push(new Space([i, j]));
+      }
+    }
+    return board;
+  }
+
+  placeShip(shipType, direction, location) {
+    // create ship object
+    let ship = new ShipCreator(shipType);
+
+    let shipLength = ship.length;
+
+    // if ship is placed such that it falls out of bounds on the board then reject placement
+    if ((direction === 'y') && ((location[0] + shipLength) < 7)) {
+      // ship placement is valid so add it to the player's fleet and populate required spaces
+      this.fleet.push(ship);
+      for (let i = location[0]; i < (location[0] + shipLength); i++) {
+        this.board[i][location[1]].occupied = ship;
+      }
+    }
+    else if ((direction === 'x') && ((location[1] + shipLength) < 7)) {
+      // same as above but alternate axis
+      this.fleet.push(ship);
+      for (let i = location[1]; i < (location[1] + shipLength); i++) {
+        this.board[location[0]][i].occupied = ship;
+      }
+    }
+    else {
+      return false;
+    }
+
+  }
+
+  receiveAttack (space) {
+
+  }
+}
+
+module.exports.GameBoard = GameBoard;
--- test/battleship.test.js
+++ test/battleship.test.js
@@ -1,9 +1,10 @@
 const gameLogic = require('../src/battleship.js')
 
+// tests to verify ship creation and various parameters and methods function properly
 const battleship = new gameLogic.ShipCreator('battleship');
 
 test('Create a battleship object', () => {
-  expect(new gameLogic.ShipCreator('battleship')).toBeDefined()
+  expect(new gameLogic.ShipCreator('battleship')).toBeDefined();
 });
 
 test('Check shipType parameter for battleship', () => {
@@ -31,10 +32,12 @@ test('Check if the ship is sunk after multiple hits', () => {
   expect(battleship.sunk).toBeTruthy();
 });
 
+
+// tests that ship creator works for other ship types
 const destroyer = new gameLogic.ShipCreator('destroyer');
 
 test('Create a destroyer object', () => {
-  expect(new gameLogic.ShipCreator('destroyer')).toBeDefined()
+  expect(new gameLogic.ShipCreator('destroyer')).toBeDefined();
 });
 
 test('Check shipType parameter for battleship', () => {
@@ -45,4 +48,45 @@ test('Check if the ship is sunk after multiple hits', () => {
   destroyer.hit();
   destroyer.hit();
   expect(destroyer.sunk).toBeTruthy();
+});
+
+
+
+
+// test that the game board object class properly creates a board object
+const testBoard = new gameLogic.GameBoard('player');
+
+test('Create a game board', () => {
+  expect(new gameLogic.GameBoard('player')).toBeDefined();
+});
+
+test("Check that player hasn't lost yet", () => {
+  expect(testBoard.lost).toBeFalsy();
+});
+
+test('Check the name of the player', () => {
+  expect(testBoard.name).toEqual('player');
+});
+
+// test that game board properly places a ship
+testBoard.placeShip('battleship', 'x', [0,0]);
+
+test("The top left square of the board is now occupied by the player's battleship", () => {
+  expect(testBoard.board[0][0].occupied).toBeTruthy();
+});
+
+test("The player's battleship extends four spaces", () => {
+  expect(testBoard.board[0][3].occupied).toBeTruthy();
+});
+
+test("The player's battleship stops at four spaces", () => {
+  expect(testBoard.board[0][4].occupied).toBeFalsy();
+});
+
+test("The player's battleship doesn't travel down the y-axis", () => {
+  expect(testBoard.board[1][0].occupied).toBeFalsy();
+});
+
+test("Trying to place a ship outside of the board's bounds will fail", () => {
+  expect(testBoard.placeShip('battleship', 'x', [0,5])).toBeFalsy();
 });
